ff268680b7a3c32936cf256f2367a644559f29ca
MartinB-E <martin.bonte@epitech.eu>
2025-06-05 16:45:21 +0200

    feat(Makefile): add .so for python logger

--- Ai/src/startAi.py
+++ Ai/src/startAi.py
@@ -6,7 +6,23 @@
 ## zappy_ai
 ##
 
+import ctypes
+
 from Ai.src.Core import core
+from enum import IntEnum
+
+class Output(IntEnum):
+    CONSOLE = 0
+    FILE_OUTPUT = 1
+    BOTH = 2
+
+logger = ctypes.CDLL('Debug/libLogger.so')
+
+logger.logger_info.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_bool]
+logger.logger_info.restype = ctypes.c_bool
+
+logger.logger_info(b"Hello from C++!", Output.BOTH, True)
+
 
 returnValue = core()
 exit(returnValue)
