13e2ed2e53e151c37171514f3803f31e5c6c7f0b
MartinB-E <martin.bonte@epitech.eu>
2025-06-11 11:43:51 +0200

    feat(Inventory): add inventoryu update and draw

--- Gui/src/Inventory/Inventory.cpp
+++ Gui/src/Inventory/Inventory.cpp
@@ -5,6 +5,7 @@
 ** Inventory
 */
 
+#include "Logger.hpp"
 #include "Inventory.hpp"
 
 
@@ -14,8 +15,17 @@
 
 
 gui::ui::Inventory::Inventory(int screenWidth, int screenHeight)
-    : _fontSize(20), _bounds({0, static_cast<float>(screenHeight) * 0.89f, static_cast<float>(screenWidth), static_cast<float>(screenHeight) / 5.0f})
+    : _fontSize(30), _bounds({0, static_cast<float>(screenHeight) * 0.89f, static_cast<float>(screenWidth), static_cast<float>(screenHeight) / 5.0f})
 {
+    _items = {
+        {"Food", {0, BROWN}},
+        {"Linemate", {0, YELLOW}},
+        {"Deraumere", {0, GREEN}},
+        {"Sibur", {0, RED}},
+        {"Mendiane", {0, WHITE}},
+        {"Phiras", {0, BLUE}},
+        {"Thystame", {0, PURPLE}}
+    };
 }
 
 gui::ui::Inventory::~Inventory()
@@ -33,6 +43,16 @@ void gui::ui::Inventory::draw()
     DrawRectangleRec(_bounds, {255, 255, 255, 50});
     DrawRectangleLinesEx(_bounds, 2, BLACK);
     DrawText("Inventory", _bounds.x + 10, _bounds.y + 10, _fontSize, BLACK);
+    float itemWidth = _bounds.width / static_cast<float>(_items.size());
+    int index = 0;
+    for (const auto& item : _items) {
+        std::string text = item.first + ": " + std::to_string(item.second.first);
+        Vector2 textSize = MeasureTextEx(GetFontDefault(), text.c_str(), _fontSize, 1);
+        float textX = _bounds.x + index * itemWidth + (itemWidth - textSize.x) / 2;
+        float textY = _bounds.y + (_bounds.height - textSize.y) / 2;
+        DrawText(text.c_str(), static_cast<int>(textX), static_cast<int>(textY), _fontSize, item.second.second);
+        index++;
+    }
 }
 
 
@@ -46,6 +66,12 @@ void gui::ui::Inventory::setBounds(Rectangle bounds)
     _bounds = bounds;
 }
 
+void gui::ui::Inventory::setInventoryItem(const std::string& item, int quantity)
+{
+    if (_items.find(item) != _items.end()) {
+        _items[item].first = quantity;
+    }
+}
 
 /************************************************************
 **                   >>>>   GETTERS   <<<<                 **
@@ -55,4 +81,14 @@ void gui::ui::Inventory::setBounds(Rectangle bounds)
 Rectangle gui::ui::Inventory::getBounds() const
 {
     return _bounds;
+}
+
+int gui::ui::Inventory::getInventoryItem(const std::string& item) const
+{
+    auto it = _items.find(item);
+
+    if (it != _items.end()) {
+        return it->second.first;
+    }
+    return 0;
 }
--- Gui/src/Inventory/Inventory.hpp
+++ Gui/src/Inventory/Inventory.hpp
@@ -8,6 +8,7 @@
 #ifndef INVENTORY_HPP_
     #define INVENTORY_HPP_
 
+    #include <map>
     #include <string>
     #include <raylib.h>
 
@@ -38,8 +39,25 @@ namespace gui {
                  */
                 Rectangle getBounds() const;
         
+                /**
+                 * @brief Set the quantity of an item in the inventory.
+                 * This function allows you to set the quantity of a specific item in the inventory.
+                 * @param item The name of the item.
+                 * @param quantity The quantity of the item to set.
+                 */
+                void setInventoryItem(const std::string& item, int quantity);
+
+                /**
+                 * @brief Get the quantity of an item in the inventory.
+                 * This function returns the quantity of a specific item in the inventory.
+                 * @param item The name of the item.
+                 * @return The quantity of the item, or 0 if the item does not exist.
+                 */
+                int getInventoryItem(const std::string& item) const;
+
             private:
                 int _fontSize;
+                std::map<std::string, std::pair<int, Color>> _items;
 
                 Rectangle _bounds;
         };
