ec6563dd6a50da4ca3073e335d8dbc02cc9d0bfe
MartinB-E <martin.bonte@epitech.eu>
2025-06-12 12:36:06 +0200

    doc(AEntity.hpp): add doxygen

--- Gui/src/AEntity/AEntity.hpp
+++ Gui/src/AEntity/AEntity.hpp
@@ -17,15 +17,59 @@ namespace gui {
         AEntity(Vector3 position, float scale, Color color);
         ~AEntity();
 
+        /**
+         * @brief Getters for the color of the entity.
+         * @return Color of the entity.
+         */
         Color getColor() override;
+
+        /**
+         * @brief Getters for the model of the entity.
+         * @return Model of the entity.
+         */
         Model getModel() override;
+
+        /**
+         * @brief Getters for the scale of the entity.
+         * @return Scale of the entity.
+         */
         float getScale() override;
+
+        /**
+         * @brief Getters for the position of the entity.
+         * @return Position of the entity.
+         */
         Vector3 getPosition() override;
+
+        /**
+         * @brief Getters for the type of the entity.
+         * @return Type of the entity as a string.
+         */
         std::string getType() override;
 
+
+        /**
+         * @brief Setters for the color of the entity.
+         * @param color Color to set for the entity.
+         */
         void setColor(Color color) override;
+
+        /**
+         * @brief Setters for the model of the entity.
+         * @param model Model to set for the entity.
+         */
         void setScale(float scale) override;
+
+        /**
+         * @brief Setters for the position of the entity.
+         * @param position Position to set for the entity.
+         */
         void setModel(Model model) override;
+
+        /**
+         * @brief Setters for the position of the entity.
+         * @param position Position to set for the entity.
+         */
         void setPosition(Vector3 position) override;
 
     protected:
