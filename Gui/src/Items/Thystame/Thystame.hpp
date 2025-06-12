ecadf5f0fc5c4de11f1649a7cc83221f84d2d8b9
MartinB-E <martin.bonte@epitech.eu>
2025-06-12 11:56:26 +0200

    feat(Thystame): add assets and to gui

---
+++ Gui/src/Items/Thystame/Thystame.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** Thystame
+*/
+
+#ifndef ZAPPY_THYSTAME_HPP
+    #define ZAPPY_THYSTAME_HPP
+
+    #include "AItem.hpp"
+
+namespace gui {
+    class Thystame : public AItem {
+        public:
+            Thystame(Vector3 position, float scale, Color color);
+            ~Thystame();
+
+            std::string getType() override;
+    };
+}
+
+#endif
