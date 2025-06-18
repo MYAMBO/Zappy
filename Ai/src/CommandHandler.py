d7a03de570448807128fd083d9cecb74063e59dc
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-06-18 16:10:20 +0200

    feat(CommandHandler): add function and tests to rectify path after being pushed

--- Ai/src/CommandHandler.py
+++ Ai/src/CommandHandler.py
@@ -21,6 +21,57 @@ def handle_inventory_string(string):
     return result
 
 
+def manage_trantorian_turn(dir1, dir2, command_list, i):
+    if dir1 in command_list:
+        i -= 1
+        if command_list[i] == "Forward" and command_list[i - 1] == dir1:
+            command_list.pop(i)
+            command_list.pop(i - 1)
+        else:
+            i -= 1
+            command_list.pop(i)
+    elif dir2 in command_list:
+        command_list.insert(i, "Forward")
+    else:
+        command_list.insert(i, dir2)
+        command_list.insert(i + 1, "Forward")
+    return command_list
+
+
+def handle_eject_command(eject_command, command_list):
+    try:
+        tile = int(eject_command.split(' ')[1])
+    except:
+        print("Invalid eject command !")
+        return command_list
+
+    i = len(command_list) - 1
+    while i >= 0:
+        if command_list[i] == "Forward":
+            break
+        i -= 1
+    i += 1
+
+    if tile == 1:
+        command_list.insert(0, "Forward")
+    elif tile == 5:
+        if command_list[0] == "Forward":
+            command_list.remove("Forward")
+        elif "Forward" not in command_list:
+            command_list.insert(0, "Right")
+            command_list.insert(1, "Right")
+            command_list.insert(2, "Forward")
+        else:
+            command_list.insert(i, command_list[0])
+            command_list.insert(i + 1, "Forward")
+    else:
+        if tile == 3:
+            command_list = manage_trantorian_turn("Right", "Left", command_list, i)
+        elif tile == 7:
+            command_list = manage_trantorian_turn("Left", "Right", command_list, i)
+    return command_list
+
+
 def handle_command(command, reply, ai):
     if reply == "ko\n" or reply.startswith("message"):
         return
