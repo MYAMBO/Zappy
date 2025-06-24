6c182059e6cf44418649b8d47a0db37b9d0483e7
Antoine Orange <antoine.orange@epitech.eu>
2025-06-24 20:05:52 +0200

    feat(Server): plv command implementation

---
+++ Server/src/commands/command_plv.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2025
+** command_plv.c
+** File description:
+** command_plv.c
+** created and edited by antoiix.
+** All rights reserved
+*/
+
+#include <unistd.h>
+
+#include "commands.h"
+#include "garbage.h"
+#include "player_informations_protocol.h"
+#include "server.h"
+#include "utils.h"
+
+static int send_plv_command(server_t *server,
+                            poll_handling_t *node, char **args)
+{
+    poll_handling_t *tmp;
+    long int id = 0;
+    char *str;
+    char *bad;
+
+    id = strtol(args[1] + 1, &bad, 10);
+    tmp = search_player_node((int)id, server);
+    if (tmp == NULL || bad == args[1] + 1) {
+        write(node->poll_fd.fd, "sbp\n", 4);
+        return SUCCESS;
+    }
+    str = get_player_level(tmp->player);
+    if (str == NULL)
+        return FAILURE;
+    write(node->poll_fd.fd, str, strlen(str));
+    my_free(str);
+    return SUCCESS;
+}
+
+int plv_command(server_t *server, poll_handling_t *node, char **args)
+{
+    if (args == NULL)
+        return FAILURE;
+    if (array_len(args) != 2 || strlen(args[1]) < 2 ||
+        args[1][0] != '#') {
+        write(node->poll_fd.fd, "sbp\n", 4);
+        return SUCCESS;
+        }
+    return send_plv_command(server, node, args);
+}
