aef034e9d0c8b84d0c88c323d5151c2445918ff1
clementaugustinowick <clement.augustinowick@epitech.eu>
2025-06-26 11:21:06 +0200

    fix(test_follow_message): return value in test after modify functions

--- tests/Ai/test_find_better_way.py
+++ tests/Ai/test_find_better_way.py
@@ -15,7 +15,7 @@ def test_get_better_way_to_resources():
     resources = [['rock'], ['big-rock', 'apple'], [], ['rock', 'food', 'apple'], ['big-rock'], ['apple', 'big-rock', 'rock'], ['food', 'apple'], ['rock', 'big-rock', 'food'], ['apple']]
     assert get_better_way_to_resources(sorted_tiles, resources, ['food']) == ['Forward', 'Forward', 'Take food']
     assert get_better_way_to_resources(sorted_tiles, resources, None) == ['Forward', 'Right', 'Forward', 'Take rock', 'Take food', 'Take apple']
-    assert get_better_way_to_resources(sorted_tiles, resources, ['gold']) == ["Right"]
+    assert get_better_way_to_resources(sorted_tiles, resources, ['gold']) == ["Right", "Forward"]
     assert get_better_way_to_resources(sorted_tiles, resources, ['gold', 'apple']) == ["Forward", "Forward", "Take apple"]
     sorted_tiles = [[0], [2], [6], [3, 1], [7, 5], [4]]
     assert get_better_way_to_resources(sorted_tiles, resources, ['food']) == None
--- tests/Ai/test_follow_message.py
+++ tests/Ai/test_follow_message.py
@@ -8,9 +8,9 @@
 from Message import follow_message
 
 def test_follow_message():
-    assert follow_message(0) == None
+    assert follow_message(0) == []
     assert follow_message(1) == ["Forward"]
-    assert follow_message(2) == ["Forward", "Left", "Forward"]
-    assert follow_message(5) == ["Right", "Right", "Forward"]
-    assert follow_message(6) == ["Right", "Forward", "Right", "Forward"]
-    assert follow_message(10) == None
+    assert follow_message(2) == ["Forward"]
+    assert follow_message(5) == ["Left", "Left", "Forward"]
+    assert follow_message(6) == ["Right", "Forward"]
+    assert follow_message(10) == []
