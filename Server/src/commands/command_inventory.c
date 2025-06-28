060369e558aecc5ccd4c0be934911e05e494c0a8
Antoine Orange <antoine.orange@epitech.eu>
2025-06-28 13:09:15 +0200

    style(lines): command_inventory

--- Server/src/commands/command_inventory.c
+++ Server/src/commands/command_inventory.c
@@ -14,12 +14,14 @@
 
 int inventory_command(server_t *server, poll_handling_t *node, char **args)
 {
+    char *str;
+
     (void)server;
     if (array_len(args) != 1) {
         write(node->poll_fd.fd, "ko\n", 3);
         return SUCCESS;
     }
-    char *str = inventory_communication(node->player);
+    str = inventory_communication(node->player);
     if (str == NULL)
         return FAILURE;
     write(node->poll_fd.fd, str, strlen(str));
