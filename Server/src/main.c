5a27419b5c8c10ff00f95b3948b3146406499519
Antoine Orange <antoine.orange@epitech.eu>
2025-06-28 13:15:24 +0200

    style(comments): main

--- Server/src/main.c
+++ Server/src/main.c
@@ -51,39 +51,6 @@ map_t *init_map(int width, int height)
     }
     return map;
 }
-//
-////create one AI to test
-//
-//ai_stats_t *create_test_ai(int id, const char *team_name, server_t *server)
-//{
-//    ai_stats_t *ai = malloc(sizeof(ai_stats_t));
-//
-//    if (!ai)
-//        return NULL;
-//    ai->connected = true;
-//    ai->fd = id + 3;
-//    ai->tmp_command = NULL;
-//    ai->id = id;
-//    ai->life = 126;
-//    ai->x = server->map_width - 1;
-//    ai->y = server->map_height - 1;
-//    ai->direction = SOUTH;
-//    ai->level = 1;
-//    ai->team_name = strdup(team_name);
-//    ai->nb_food = 3;
-//    ai->nb_linemate = 1;
-//    ai->nb_deraumere = 6;
-//    ai->nb_sibur = 5;
-//    ai->nb_mendiane = 8;
-//    ai->nb_phiras = 2;
-//    ai->nb_thystame = 3;
-//    ai->in_incantation = false;
-//    for (int i = 0; i < 7; i++)
-//        ai->inventory.resources[i] = rand() % 3;
-//
-//    return ai;
-//}
-
 
 int parse_arguments(int ac, char **av, server_t *server)
 {
