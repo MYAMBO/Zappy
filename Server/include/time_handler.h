7ca1104a88d093eb6ceac9d72732d435b57d9e2b
Antoine Orange <antoine.orange@epitech.eu>
2025-06-28 13:07:25 +0200

    feat(time): time handler implementation

---
+++ Server/include/time_handler.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** time_handler.h
+** File description:
+** time_handler.h
+** created and edited by antoiix.
+** All rights reserved
+*/
+
+#ifndef TIME_HANDLER_H
+    #define TIME_HANDLER_H
+
+    #include "struct.h"
+
+bool increase_tick(server_t *server);
+
+#endif //TIME_HANDLER_H
