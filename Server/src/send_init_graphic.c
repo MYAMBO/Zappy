d6e6ba34db5c4765e0a79ff036442973a9d15077
Antoine Orange <antoine.orange@epitech.eu>
2025-06-30 21:34:30 +0200

    feat(Server): send commands in running for graphic user

--- Server/src/send_init_graphic.c
+++ Server/src/send_init_graphic.c
@@ -13,8 +13,10 @@
 #include "commands.h"
 #include "egg_protocol.h"
 #include "garbage.h"
+#include "player_connection_protocol.h"
 #include "server.h"
 #include "technical_protocol.h"
+#include "utils.h"
 
 int launch_eggs_display(poll_handling_t *node, slot_table_t *table)
 {
@@ -31,8 +33,49 @@ int launch_eggs_display(poll_handling_t *node, slot_table_t *table)
     return SUCCESS;
 }
 
+int send_pnw(server_t *server, poll_handling_t *node)
+{
+    char *str = player_new_connection(node->player);
+
+    if (str == NULL)
+        return FAILURE;
+    if (send_message_graphic(server, str) == FAILURE)
+        return FAILURE;
+    my_free(str);
+    return SUCCESS;
+}
+
+int send_player_info(server_t *server, poll_handling_t *node, char **args)
+{
+    int a;
+
+    for (poll_handling_t *tmp = server->poll_list; tmp; tmp = tmp->next) {
+        if (!tmp->player || strcmp(tmp->player->team_name, "GRAPHIC") == 0 ||
+            !tmp->player->connected)
+            continue;
+        my_free(args[1]);
+        a = snprintf(NULL, 0, "#%d", tmp->player->id);
+        args[1] = my_malloc(a + 1);
+        sprintf(args[1], "#%d", tmp->player->id);
+        if (send_pnw(server, tmp) == FAILURE ||
+            pin_command(server, node, args) == FAILURE ||
+            plv_command(server, node, args) == FAILURE ||
+            ppo_command(server, node, args) == FAILURE)
+            return FAILURE;
+    }
+    my_free_array(args);
+    return SUCCESS;
+}
+
 int send_graphic_suite(server_t *server, poll_handling_t *node)
 {
+    char **args = my_malloc(sizeof(char *) * 3);
+
+    if (args == NULL)
+        return FAILURE;
+    args[0] = "command";
+    args[1] = NULL;
+    args[2] = NULL;
     for (int i = 0; server->team_names[i] != NULL; i++) {
         if (strcmp(server->team_names[i]->name, "GRAPHIC") == 0)
             continue;
@@ -40,6 +83,8 @@ int send_graphic_suite(server_t *server, poll_handling_t *node)
             server->team_names[i]) == FAILURE)
             return FAILURE;
     }
+    if (send_player_info(server, node, args) == FAILURE)
+        return FAILURE;
     return SUCCESS;
 }
 
